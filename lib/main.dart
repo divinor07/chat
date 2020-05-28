1ea052f85d42d0344a548d49360d29c75d434cee
Divino Rodrigues <divino.sr@hotmail.com>
2020-05-28 15:24:02 -0300

    Configurando o Projeto

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+void main() {
+  runApp(MyApp());
+
+  Firestore.instance
+      .collection("col")
+      .document("doc")
+      .setData({"texto": "Divino"});
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Container(),
+    );
+  }
+}
